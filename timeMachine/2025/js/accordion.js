bc2c4f7c5dcbfa721f5430aa13b8a86498060539
achen05 <72044476+achen05@users.noreply.github.com>
2026-06-12 21:16:14 -0400

    Delete timeMachine directory

--- timeMachine/2025/js/accordion.js
+++
@@ -1,7 +0,0 @@
-const wrapper = document.querySelector('.paragraph-wrapper');
-const toggleBtn = document.getElementById('toggle');
-
-toggleBtn.addEventListener('click', () => {
-  wrapper.classList.toggle('expanded');
-  toggleBtn.textContent = wrapper.classList.contains('expanded') ? '[-] Minimize Text' : '[+] Expand Full Text';
-});
